c7b38f8f2c9e5fcd6295ec672d932c76106d9159
sp-1029 <jaj666420@gmail.com>
2023-09-06 18:03:58 -0600

    Started InteractiveGreeter

---
+++ src/InteractiveGreeter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InteractiveGreeter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Where are you from?");
+        String place = scanner.nextLine();
+        System.out.println("How old are you? (years)");
+        int age = scanner.nextInt();
+        System.out.println("Hello " + name + " from " + place + ", you are " + age + " years old");
+        System.out.println("What is your favorite activity?");
+        String activity = scanner.nextLine();
+        System.out.println("Have fun the next time you " + activity + "!");
+        System.out.println("How much would you like to donate today? (dollars)");
+        double donate = scanner.nextDouble();
+        System.out.println("you will donate $" + donate + " today, yay!");
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("wow cool date");
-        System.out.println(System.out);
+
     }
 }
