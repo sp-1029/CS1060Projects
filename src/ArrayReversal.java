107ac546884087f95022ac65cec5d07f5f9e3634
sp-1029 <jaj666420@gmail.com>
2023-10-25 17:29:40 -0600

    reformatted

--- src/ArrayReversal.java
+++ src/ArrayReversal.java
@@ -7,10 +7,10 @@ public class ArrayReversal {
         int addToArray;
         int length = 0;
         System.out.println("Type positive integers and I will put them in an array, don't go over 100, I will end once you type in '-1' and print the array backwards");
-        for(int i = 0; i <= 99; i++) {
+        for (int i = 0; i <= 99; i++) {
             System.out.println("next number:");
             addToArray = scanner.nextInt();
-            if(addToArray == -1){
+            if (addToArray == -1) {
                 break;
             } else if (addToArray < 0) {
                 System.out.println("Invalid number");
@@ -20,7 +20,7 @@ public class ArrayReversal {
                 length++;
             }
         }
-        for(int i = length - 1; i >= 0; i--) {
+        for (int i = length - 1; i >= 0; i--) {
             System.out.println(array[i]);
         }
     }
